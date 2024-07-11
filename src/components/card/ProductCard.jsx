5ed5a6b3aae6982d8da04c394bdb10e16c46bbeb
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-07-11 11:24:30 +0500

    image size

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -2,7 +2,7 @@ const ProductCard = ({ id, category, filter, image, name, description, price }) 
     return (
         <div className="w-full sm:w-[300px] items-center h-auto flex flex-row sm:flex-col justify-between border rounded-[12px] bg-white mb-[30px] ownCard overflow-hidden">
             <div className="relative mb-1 py-2 sm:py-0 h-full flex justify-center items-center sm:w-auto w-[30%]">
-                <img src={image} className="w-[150px] object-cover sm:w-auto sm:h-auto" alt="" />
+                <img src={image} className="w-[200px] object-cover sm:w-auto sm:h-auto" alt="" />
                 {filter ? <div className="absolute top-2 sm:top-5 left-0 px-2 py-[2px] sm:px-4 sm:py-[7px] bg-[#E23535] text-white rounded-tr-[8px] rounded-br-[8px]"><span className="text-[12px] sm:text-[14px] uppercase">{filter}</span></div> : ''}
             </div>
             <div className="p-3 sm:p-5 w-[70%] sm:w-auto">
